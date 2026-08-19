7bb171461ce4a7a26282b88aa7b19752509e45f9
Ravi Peters <ravi@ylan.nl>
2026-08-19 18:43:21 +0100

    disable popover animations

--- src/components/ui/popover.tsx
+++ src/components/ui/popover.tsx
@@ -37,7 +37,7 @@ function PopoverContent({
       >
         <PopoverPrimitive.Popup
           className={cn(
-            'z-50 rounded-sm border border-border bg-popover p-2 text-popover-foreground shadow-lg outline-none transition-[opacity,transform] data-starting-style:opacity-0 data-starting-style:scale-95 data-ending-style:opacity-0 data-ending-style:scale-95 data-[side=bottom]:data-starting-style:-translate-y-2 data-[side=bottom]:data-ending-style:-translate-y-2 data-[side=inline-end]:data-starting-style:-translate-x-2 data-[side=inline-end]:data-ending-style:-translate-x-2 data-[side=inline-start]:data-starting-style:translate-x-2 data-[side=inline-start]:data-ending-style:translate-x-2 data-[side=left]:data-starting-style:translate-x-2 data-[side=left]:data-ending-style:translate-x-2 data-[side=right]:data-starting-style:-translate-x-2 data-[side=right]:data-ending-style:-translate-x-2 data-[side=top]:data-starting-style:translate-y-2 data-[side=top]:data-ending-style:translate-y-2',
+            'z-50 rounded-sm border border-border bg-popover p-2 text-popover-foreground shadow-lg outline-none',
             className,
           )}
           {...props}
